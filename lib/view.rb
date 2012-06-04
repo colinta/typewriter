641feb86da3577aea4a6980be323cc282d376e36
Colin Thomas-Arnold <colinta@gmail.com>
2012-06-04 10:30:55 -0600

    app_launcher initial commit

---
+++ lib/view.rb
@@ -0,0 +1,24 @@
+
+class AppLauncherView < UIView
+
+  def apps
+    @apps ||= []
+  end
+
+  ##|
+  ##|  STYLES
+  ##|  easily stylable using teacup
+  ##|   * backgroundColor is already supported by UIView
+  ##|
+
+  ##|
+  ##|  DRAWING
+  ##|
+  def drawRect(rect)
+    super
+  end
+
+end
+
+module AppLauncherDelegate
+end
