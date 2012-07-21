0e3eaa08b39ec129479b1802c5d30c59f3df36e8
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-21 08:43:48 -0600

    margin fixes, turn centering off by default

--- lib/typewriter/view.rb
+++ lib/typewriter/view.rb
@@ -10,12 +10,16 @@ class TypewriterView < UIView
   attr_accessor :centered
   attr_accessor :min_width, :min_height
 
+  def init
+    self.initWithFrame([[0, 0], [0, 0]])
+  end
+
   def initWithFrame(frame)
     super
 
     self.spacing = 0
     self.margin = 0
-    self.centered = true
+    self.centered = false
     self.min_width = nil
     self.min_height = nil
 
@@ -41,58 +45,104 @@ class TypewriterView < UIView
       @horizontal_spacing = @vertical_spacing = spacing
     end
   end
-
   # Accepts an integer, or an array of integers.  Follows CSS-like assignment:
   #
   # 1 argument => top==bottom==left==right
   # 2 arguments => top==bottom, left==right
   # 3 arguments => top, bottom, left==right
   # 4 arguments => top, bottom, left, right
-  def margin=(margin)
-    if Array === margin
-      case margin.length
-      when 1
-        self.margin = margin[0]
-      when 2
-        @top_margin = margin[0]
-        @bottom_margin = margin[0]
-        @left_margin = margin[1]
-        @right_margin = margin[1]
-      when 3
-        @top_margin = margin[0]
-        @bottom_margin = margin[2]
-        @left_margin = margin[1]
-        @right_margin = margin[1]
-      when 4
-        @top_margin = margin[0]
-        @bottom_margin = margin[1]
-        @left_margin = margin[2]
-        @right_margin = margin[3]
-      end
+  def margin=(margins)
+    return margins = [margins] unless margins.is_a? Array
+
+    case margins.length
+    when 1
+      @top_margin = @bottom_margin = @left_margin = @right_margin = margins[0]
+    when 2
+      @top_margin = margins[0]
+      @bottom_margin = margins[0]
+      @left_margin = margins[1]
+      @right_margin = margins[1]
+    when 3
+      @top_margin = margins[0]
+      @bottom_margin = margins[2]
+      @left_margin = margins[1]
+      @right_margin = margins[1]
+    when 4
+      @top_margin = margins[0]
+      @bottom_margin = margins[1]
+      @left_margin = margins[2]
+      @right_margin = margins[3]
     else
-      @top_margin = @bottom_margin = @left_margin = @right_margin = margin
+      raise "Too many arguments (#{margins.length}) sent to MarginView#margin"
     end
+
+    margins
   end
 
+  ##|  DEFAULTS
+  def vertical_spacing
+    @vertical_spacing ||= 0
+    @vertical_spacing
+  end
+  def horizontal_spacing
+    @horizontal_spacing ||= 0
+    @horizontal_spacing
+  end
+  def top_margin
+    @top_margin ||= 0
+    @top_margin
+  end
+  def bottom_margin
+    @bottom_margin ||= 0
+    @bottom_margin
+  end
+  def left_margin
+    @left_margin ||= 0
+    @left_margin
+  end
+  def right_margin
+    @right_margin ||= 0
+    @right_margin
+  end
+
+  ##|
+  ##|  START AT 0, 0, AND START FLOATING
+  ##|
+  def layoutSubviews
+    # the max_height of *all* the rows so far (not just the current row)
+    @max_height = top_margin
+    clear
+
+    # when a row would be longer than this, it is wrapped to the next row.
+    @max_x = self.frame.size.width - right_margin
+
+    self.subviews.each do |view|
+      view.layoutSubviews
+      add_next(view)
+    end
+    clear
+  end
+
+  private
   def clear
-    @x = @left_margin
+    @x = left_margin
     @y = @max_height
     # only add the horizontal_spacing after at least one row has been written
-    if @y > @top_margin
-      @y += @vertical_spacing
+    if @y > top_margin
+      @y += vertical_spacing
     end
 
     if self.centered
-      row_width = @left_margin
+      row_width = left_margin
       row_height = min_height || 0
       @row.each do |view|
         if row_width > 0
-          row_width += @horizontal_spacing
+          row_width += horizontal_spacing
         end
         row_height = view.frame.size.height if view.frame.size.height > row_height
         row_width += view.frame.size.width
       end
-      row_width += @right_margin
+      row_width += right_margin
       row_width = min_width if min_width and row_width < min_width
       x = ((self.frame.size.width - row_width) / 2).round
 
@@ -111,23 +161,6 @@ class TypewriterView < UIView
     @row = []
   end
 
-  ##|
-  ##|  START AT 0, 0, AND START FLOATING
-  ##|
-  def layoutSubviews
-    # the max_height of *all* the rows so far (not just the current row)
-    @max_height = @top_margin
-    self.clear
-
-    # when a row would be longer than this, it is wrapped to the next row.
-    @max_x = self.frame.size.width - @right_margin
-
-    self.subviews.each do |view|
-      add_next(view)
-    end
-    self.clear
-  end
-
   def add_next(view)
     view_frame = view.frame
 
@@ -138,8 +171,9 @@ class TypewriterView < UIView
     end
 
     next_x = @x + width
+    # too big?
     if next_x > @max_x
-      self.clear
+      clear
       next_x = @x + width
     end
 
@@ -152,8 +186,8 @@ class TypewriterView < UIView
     @max_height = next_y if next_y > @max_height
 
     view_frame.origin.x = @x
-    @x = next_x + @horizontal_spacing
     view_frame.origin.y = @y
+    @x = next_x + horizontal_spacing
 
     view.frame = view_frame
     @row.push view
